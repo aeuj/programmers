357093e08113bcf099b7e0a66101e22acc6db7d4
aeuj <117497129+aeuj@users.noreply.github.com>
2023-01-23 23:33:18 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,3 +1,7 @@
-function solution(num1, num2) {
-    return num1 === num2 ? 1 : -1;
+function solution (num1, num2){
+  if (num1 === num2){
+    return 1;
+  }else if(num1 !== num2){
+    return -1;
+  }
 }
